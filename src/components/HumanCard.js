d9c29ba257836084ac7b5d3d5f186a0efc31ccb4
tunkhine126 <tunkhine126@gmail.com>
2019-07-07 17:34:25 -0400

    Profile card and route logic

--- src/components/HumanCard.js
+++ src/components/HumanCard.js
@@ -5,20 +5,21 @@ const HumanCard = props =>{
 
   return (
     <div className='humanCard'> 
-    <div className="ui column">
-      <div className="ui card">
-        <div className="image">
-          <img alt="oh no!" src={"https://i.imgur.com/1DLxwJZ.jpg"} />
-        </div>
-        <div className="content">
-          <div className="header">
-            {"Human"}
+      <h1>{props.user.username}</h1>
+      <div className="ui column">
+        <div className="ui card">
+          <div className="image">
+            <img src={props.user.img_url} alt="profile image"  />
           </div>
+          <div className="content">
+            <div className="header">
+              {"Human"}
+            </div>
 
-          <div className="meta text-wrap">
-            <small>{"I'm alive"}</small>
+            <div className="meta text-wrap">
+              <small>{"I'm alive"}</small>
+            </div>
           </div>
-        </div>
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import CreateUser from './components/CreateUser'
+import Profile from './components/Profile'
 import Quiz from './components/Quiz'
-import HumanCard from './components/HumanCard'
-import ZombieCard from './components/ZombieCard'
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
@@ -13,7 +12,6 @@ const Application = () => {
 
   return (
     <App />
-  
   )
 }
 
@@ -29,25 +27,13 @@ const quiz = () => {
   )
 }
 
-const humanProfile = () => {
-  return (
-    <HumanCard />
-  )
-}
-
-const zombieProfile = () => {
-  return (
-    <ZombieCard />
-  )
-}
 ReactDOM.render((
   <Router>
     <React.Fragment>
       <Route path="/" render={Application} />
       <Route exact path="/CreateUser" render={newUserForm} />
       <Route exact path="/Quiz" render={quiz} />
-      <Route exact path="/human" render={humanProfile} />
-      <Route exact path="/zombie" render={zombieProfile} />
+      <Route exact path="/profile/:id" component={Profile} />
     </React.Fragment>
   </Router>),
   document.getElementById('root')
